7fcf05e1b058c204e5569b1399d67d4009a5f2f5
lalarustamli <rustamlilala@gmail.com>
2019-11-19 10:55:16 +0400

    flasked

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,16 @@ import telegram
 
 @app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    print 'Hello, World!'
 
 
-@app.route("/webhook", methods=['GET', 'POST'])
-def listen():
-    if request.method == "POST":
-        text = ''
-        chat_id, text, update_id = bot.get_last_id_text(get_updates())
-        chat_id, text, update_id = bot.start(chat_id)
-        print('Started')
+# @app.route("/webhook", methods=['GET', 'POST'])
+# def listen():
+#     if request.method == "POST":
+#         text = ''
+#         chat_id, text, update_id = bot.get_last_id_text(get_updates())
+#         chat_id, text, update_id = bot.start(chat_id)
+#         print('Started')
     # if request.method == "GET":
     #     return chatbot.verify_webhook(request)
 
